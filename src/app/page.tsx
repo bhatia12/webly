da839b99e5d902ab0210d5e31dd83ecafa371f28
Shubham Bhatia <shubham.bhatia@schoolnetindia.com>
2025-07-05 13:53:41 +0530

    05:make ai agents workable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 
 const Page = () => {
+  const [value, setValue] = useState("")
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background Job Started")
+    }
+  }));
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={()=> invoke.mutate({text: "John"})}>
+      <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+      <Button onClick={()=> invoke.mutate({value: value})}>
         Invoke Background Job
       </Button>
     </div>
