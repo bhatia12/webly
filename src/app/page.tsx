e430e0c10cd5a8ca7e32b24950e5c909588fa512
Shubham Bhatia <shubham.bhatia@schoolnetindia.com>
2025-07-06 02:59:13 +0530

    08:databaseQuery and messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client"
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -11,15 +11,16 @@ import { toast } from "sonner";
 const Page = () => {
   const [value, setValue] = useState("")
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions())
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
-      toast.success("Background Job Started")
+      toast.success("Message Created")
     }
   }));
   return (
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
-      <Button onClick={()=> invoke.mutate({value: value})}>
+      <Button disabled={createMessage.isPending} onClick={()=> createMessage.mutate({value: value})}>
         Invoke Background Job
       </Button>
     </div>
