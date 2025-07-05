d0c78ce262785576a41a51972c34789d09612bb1
Shubham Bhatia <shubham.bhatia@schoolnetindia.com>
2025-07-05 21:24:40 +0530

    06: setup_sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
-import {  openai, createAgent } from "@inngest/agent-kit";
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import {Sandbox} from '@e2b/code-interpreter'
+import { getSandbox } from "./utils";
 
 // Step 2 code...
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async() => {
+          const sandbox = await Sandbox.create("webly-nextjs-test")
+          return sandbox.sandboxId
+        })
+
         const codeAgent = createAgent({
             name: "summarizer",
             system: "You are an expert next.js developer.  You write read able and maintainable code. you write simple next.js snippets & React",
@@ -15,7 +22,13 @@ export const helloWorld = inngest.createFunction(
             `Write the following snippets: ${event.data.value}`,
           );
           console.log(output);
-      return { output };
+          const sandboxUrl = await step.run("get-sandbox-url", async()=> {
+            const sandbox = await getSandbox(sandboxId);
+            const host =  sandbox.getHost(3000);
+            return `https://${host}`;
+
+
+          })
+          return { output, sandboxUrl };
     },
   );
-  
